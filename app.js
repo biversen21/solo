7adc2927a8fb11785baac8c41965e3fcb387771f
Brian Iversen <biversen21@gmail.com>
2014-10-13 14:32:56 -0700

    recursive html appends in basic form

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 $(function(){
+	
+	var counter = 1;
+	
   var factorial = function(num){
-		var current = num;
-		var result = 1;
+		counter++;
 		
-		var recurse = function(current){
-			result *= current;
-			return (current > 1) ? recurse(current - 1) : result;
+		$('.container').append('<h4>var current: ' + num + ' stack: </h4>');
+		
+		for (var i = 0; i < counter; i++) {
+			var content = 'factorial(' + (num - 1) + ')';
+			$('h4:nth-of-type(' + i + ')').append('<span>' + num + ' * (' + content + ')</span>');
 		}
-		var finalResult = recurse(num)
+		
+		if (num > 1) {
+			setTimeout(function(){return num * factorial(num - 1)}, 1000);
+		} else {
+			return 1;
+		}
+		
   }
 	
+  var fillResult = function(runs){
+		var subResult = 1;
+		var returnedVal;
+		
+		for (var i = 1; i <= runs; i++) {
+			subResult = subResult * i;
+		  $('h4:nth-of-type(' + (runs - i + 1) + ')').append('<span> Result: ' + subResult);
+	  }
+  }	
 	
+	$('.startFactorial').on('click', function(){
+		$('.container').empty();
+		var factorialStart = $('#factorialNum').val();
+		factorial(factorialStart);
+		// setTimeout(function(){fillResult(factorialStart)}, 5000);
+	})
 });
