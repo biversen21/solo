d3ef835dd4092f48dce54d402b7708c117e1e794
Brian Iversen <biversen21@gmail.com>
2014-10-13 15:13:35 -0700

    stack builds dynamically

--- app.js
+++ app.js
@@ -5,35 +5,32 @@ $(function(){
   var factorial = function(num){
 		counter++;
 		
-		$('.container').append('<h4>var current: ' + num + ' stack: </h4>');
-		
-		for (var i = 0; i < counter; i++) {
-			var content = 'factorial(' + (num - 1) + ')';
-			$('h4:nth-of-type(' + i + ')').append('<span>' + num + ' * (' + content + ')</span>');
-		}
 		
 		if (num > 1) {
+			$('.container').append('<h4>var current: ' + num + ' stack: <span class="newSpan' + num + '"></span></h4>');
+			for (var i = 0; i < counter; i++) {
+				$(".newSpan" + num).html(' (' + (num) + ' * <span class="newSpan' + (num -1) +'">factorial(' + (num - 1) + ') </span>)');
+			}			
 			setTimeout(function(){return num * factorial(num - 1)}, 1000);
 		} else {
+			$('.container').append('<h4>var current: 1 stack: 1');
 			return 1;
 		}
 		
   }
 	
   var fillResult = function(runs){
-		var subResult = 1;
-		var returnedVal;
-		
-		for (var i = 1; i <= runs; i++) {
-			subResult = subResult * i;
-		  $('h4:nth-of-type(' + (runs - i + 1) + ')').append('<span> Result: ' + subResult);
-	  }
+		var result = 1;
+    for (var i = 1; i <= runs; i++) {
+			result = result * i;
+    	$('.newSpan' + i).html(result);
+    }
   }	
 	
 	$('.startFactorial').on('click', function(){
 		$('.container').empty();
 		var factorialStart = $('#factorialNum').val();
 		factorial(factorialStart);
-		// setTimeout(function(){fillResult(factorialStart)}, 5000);
+		setTimeout(function(){fillResult(factorialStart)}, 5000);
 	})
 });
